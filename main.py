2dd3da8ae98ef13250bf1f81818f66dcac3e2a5b
Xaliffa <xaliffa@gmail.com>
2023-04-04 21:40:07 +0300

    Dicts etc

---
+++ main.py
@@ -0,0 +1,55 @@
+# list_1 = []
+# # list_1 = list()
+
+# # list_1 = [1, 2, 3, 8]
+# # # print(list_1)
+
+# # for i in list_1:
+# #     print(i)
+
+# print(len(list_1))
+
+# list_1 = []
+# print(list_1)
+
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
+
+# list_1.insert(2, 11)
+# print(list_1)
+
+# print(list_1[-2:])
+
+# t = ()
+# print(type(t))
+# t = (1,)
+# print(type(t))
+# t = (1, 5, 3,)
+# print(type(t))
+
+# v = [1, 8, 9]
+# print(v)
+# print(type(v))
+
+# v = tuple(v)
+# print(v)
+# print(type(v))
+
+# a, b, c = v
+# print(a, b, c)      
+
+
+# t = (1, 2, 3, 5, )
+# for i in range(len(t)):
+#     print(t[i])
+# t[0] = 2
+
+d = {}
+d = dict()
+
+d['q'] = 'qwwerty'
+print(d)
+
+d['w'] = 'wwerty'
+print(d['w'])
